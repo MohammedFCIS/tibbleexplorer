8f4fd889f965879c89bfc10e6f1cfeb95dfaf6fc
Mohammed Ali <mali@uplandsoftware.com>
2019-12-07 20:53:54 +0200

    #1, add feature classes

--- R/tibble_overview_module.R
+++ R/tibble_overview_module.R
@@ -21,6 +21,9 @@ tibble_overview_module_ui <- function(id){
     fluidRow(
       valueBoxOutput(ns("observations_number")),
       valueBoxOutput(ns("features_number"))
+    ),
+    fluidRow(
+      plotOutput(ns("feature_classes"))
     )
   )
 }
@@ -30,9 +33,19 @@ tibble_overview_module_ui <- function(id){
 #' @rdname tibble_overview_module
 #' @export
 #' @keywords internal
-    
+#' @importFrom purrr map_df
+#' @importFrom  stringr str_to_sentence
+#' @import ggplot2, plotly, dplyr
+#'   
 tibble_overview_server <- function(input, output, session){
   mytibble <- get_golem_options("mytibble")
+  tibble_classes <- map(mytibble, class) %>%
+    as_tibble() %>% 
+    filter(row_number() == n()) %>% 
+    gather("Feature", "Class") %>%
+    mutate(Class = str_to_sentence(Class)) %>% 
+    group_by(Class) %>% 
+    summarise(n = n())
   output$observations_number <- renderValueBox(
     valueBox(
       nrow(mytibble),
@@ -46,5 +59,13 @@ tibble_overview_server <- function(input, output, session){
       "Features",
       icon("columns")
     )
+  )
+  output$feature_classes <- renderPlot(
+    tibble_classes %>%
+      ggplot(aes(x = Class, y = n, fill = Class)) +
+      geom_bar(stat = "identity", alpha = .6) +
+      xlab("") +
+      ylab("Features Number") +
+      theme_bw()
   )
 }
